b27b2ebd4b53d37375d6502d89299f1a1a61ecb9
AxmetES <axmet-1984@mail.ru>
2019-10-02 15:51:36 +0600

    Coin Flip Simulation - Write some code that simulates flipping a single coin however many times the user decides. The code should record the outcomes and count the number of tails and heads. Done!

--- example_nestad_list.py
+++ example_nestad_list.py
@@ -2,7 +2,7 @@ import random
 
 list_result = []
 while True:
-    user = input('Y/N : ')
+    user = input('do you want flip a coin ? Y/N : ')
     if user == 'y':
         coin = random.randint(0, 1)
         if coin == 0:
@@ -13,4 +13,10 @@ while True:
         print(coin_pic)
     else:
         break
+count_tails = [x for x in list_result if x == 'tail']
+count_heads = [x for x in list_result if x == 'head']
+
 print(list_result)
+
+print(f' tails  = {len(count_tails)}')
+print(f' heads  = {len(count_heads)}')
